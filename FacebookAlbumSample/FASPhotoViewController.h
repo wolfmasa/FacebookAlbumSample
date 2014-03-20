d99e915f1546c563ffb225165251716b16a21ce0
Masa.J <masa@uehara-masashi-no-macbook.local>
2014-03-21 00:39:22 +0900

    Add Photo View

---
+++ FacebookAlbumSample/FASPhotoViewController.h
@@ -0,0 +1,14 @@
+//
+//  FASPhotoViewController.h
+//  FacebookAlbumSample
+//
+//  Created by J.Masa on 2014/03/21.
+//  Copyright (c) 2014年 Project Wolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FASPhotoViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ FacebookAlbumSample/FASPhotoViewController.m
@@ -0,0 +1,49 @@
+//
+//  FASPhotoViewController.m
+//  FacebookAlbumSample
+//
+//  Created by J.Masa on 2014/03/21.
+//  Copyright (c) 2014年 Project Wolf. All rights reserved.
+//
+
+#import "FASPhotoViewController.h"
+
+@interface FASPhotoViewController ()
+
+@end
+
+@implementation FASPhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
